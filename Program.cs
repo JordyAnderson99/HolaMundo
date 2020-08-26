e0effccb42f4be543bba66e8cf187fcf21225c5a
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2020-08-26 00:17:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Cual es tu nombre?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHola, {name}, y ,{date:d}, y, {date:t}!");
+            Console.Write("Precione una tecla para finalizar");
+            Console.ReadKey(true);
+        }
+    }
+}
